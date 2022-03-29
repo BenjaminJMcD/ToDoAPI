85810bd31a26cad154c95c17f2436ef3e1d39067
Benjamin McDaniel <BenjaminJMcD@outlook.com>
2022-03-29 15:45:02 -0500

    ToDo Controller fully functional

---
+++ ToDoAPI.API/Controllers/CategoriesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ToDoAPI.API.Models;
+using ToDoAPI.Data.EF;
+using System.Web.Http.Cors;
+
+namespace ToDoAPI.API.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+
+    public class CategoriesController : ApiController
+    {
+        ToDoEntities db = new ToDoEntities();
+
+
+
+
+    }//end CLASS
+}//end NAMESPACE
--- ToDoAPI.API/Controllers/ToDoController.cs
+++ ToDoAPI.API/Controllers/ToDoController.cs
@@ -16,7 +16,7 @@ namespace ToDoAPI.API.Controllers
         ToDoEntities db = new ToDoEntities();
 
         //GET - api/ToDo
-        public IHttpActionResult GetToDoItems()
+        public IHttpActionResult GetToDos()
         {
             List<ToDoViewModel> todo = db.ToDoItems.Include("Category").Select(t => new ToDoViewModel()
             {
@@ -63,7 +63,77 @@ namespace ToDoAPI.API.Controllers
             return Ok(todo);
         }
 
+        //POST - api/ToDo
+        public IHttpActionResult PostToDo(ToDoViewModel todo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Invalid Data");
+            }
 
+            ToDoItem newtodo = new ToDoItem()
+            {
+                Action = todo.Action,
+                Done = todo.Done,
+                CategoryId = todo.CategoryId
+            };
 
-    }
-}
+            db.ToDoItems.Add(newtodo);
+            db.SaveChanges();
+
+            return Ok(newtodo);
+
+        }//End PostToDo
+
+        //PUT - api/todo
+        public IHttpActionResult PutToDo(ToDoViewModel todo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Invalid Data");
+            }
+
+            ToDoItem oldToDo = db.ToDoItems.Where(t => t.ToDoId == todo.ToDoId).FirstOrDefault();
+
+            if (oldToDo != null)
+            {
+                oldToDo.ToDoId = todo.ToDoId;
+                oldToDo.Action = todo.Action;
+                oldToDo.CategoryId = todo.CategoryId;
+                db.SaveChanges();
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+
+        }//End PUTToDo
+
+        public IHttpActionResult DeleteToDo(int id)
+        {
+            ToDoItem todo = db.ToDoItems.Where(t => t.ToDoId == id).FirstOrDefault();
+
+            if (todo != null)
+            {
+                db.ToDoItems.Remove(todo);
+                db.SaveChanges();
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }// end DELETE
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+    }//end class
+}//end namespace
